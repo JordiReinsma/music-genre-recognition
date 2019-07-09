8dc1517a5bb4c643455f617df1cd4712c72ead8a
Jordi Alves Reinsma <38013106+JordiReinsma@users.noreply.github.com>
2019-07-09 14:54:01 -0300

    Update training.py

--- training.py
+++ training.py
@@ -33,7 +33,7 @@ GENRES = {'Electronic'    : 0,
 LABEL_FILE  = 'data/track_labels_small.csv'
 DATASET_DIR = 'data/fma_small_mel/'
 
-TRACKS_PER_GENRE = 40
+TRACKS_PER_GENRE = 20
 DATASET_SIZE = TRACKS_PER_GENRE * len(GENRES)
 TRAIN_SIZE = DATASET_SIZE // 2
 VALID_SIZE = DATASET_SIZE // 4
@@ -42,6 +42,7 @@ NORMALIZE_METHOD = 0
 
 BATCH_SIZE = 0.05 * TRAIN_SIZE
 TRACK_SHAPE = (128, 1288)
+TRACK_RESHAPE = (64, 320)
 EPOCHS = 10
 
 # Obtem os labels de todas as musicas do fma_small
@@ -49,6 +50,26 @@ def get_track_labels():
   file = csv.reader(open(LABEL_FILE, 'r'))
   all_labels = {int(rows[0]) : GENRES[rows[1]] for rows in file}
   return all_labels
+
+# Diminui as dimensoes do espectrograma
+# Para diminuir carga para a memoria
+def reshape_track(track):
+  oldx, oldy = TRACK_SHAPE[0], TRACK_SHAPE[1]
+  newx, newy = TRACK_RESHAPE[0], TRACK_RESHAPE[1]
+  
+  aux = np.zeros((newx, oldy))
+  newls = np.linspace(0, 1, newx)
+  oldls = np.linspace(0, 1, oldx)
+  for col in range(oldy):
+    aux[:,col] = np.interp(newls, oldls, track[:,col])
+  
+  newtrack = np.zeros((newx, newy))
+  newls = np.linspace(0, 1, newy)
+  oldls = np.linspace(0, 1, oldy)
+  for lin in range(newx):
+    newtrack[lin,:] = np.interp(newls, oldls, aux[lin,:])
+  
+  return newtrack
 
 # Dataset gerado aleatoriamente, com
 # as musicas contidas no fma_small,
@@ -96,15 +117,15 @@ def get_dataset_fma_tiny(all_labels):
     # Senao, insere musica no dataset
     # Primeiro enche o conjunto de teste
     if num_genres[genre] < TRACKS_PER_GENRE // 4:
-      test_set.append(track)
+      test_set.append(reshape_track(track))
       test_label.append(all_labels[int(file[:-4])])
     # Depois o conjunto de validacao
     elif num_genres[genre] < TRACKS_PER_GENRE // 2:
-      valid_set.append(track)
+      valid_set.append(reshape_track(track))
       valid_label.append(all_labels[int(file[:-4])])
     # E por ultimo o conjunto de treino
     else:
-      train_set.append(track)
+      train_set.append(reshape_track(track))
       train_label.append(all_labels[int(file[:-4])])
     
     num_genres[genre] += 1
@@ -129,7 +150,7 @@ def normalize(dataset):
     max = np.amax(dataset)
     dataset = [(x - min) / (max - min) for x in dataset]
   
-  dataset = np.array(dataset).reshape((-1,) + TRACK_SHAPE + (1,))
+  dataset = np.array(dataset).reshape((-1,) + TRACK_RESHAPE + (1,))
   
   return dataset
 
@@ -158,7 +179,7 @@ if __name__ == '__main__':
   
   # Criacao do modelo de rede neural
   model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(128, 1288, 1)),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(TRACK_RESHAPE + (1,))),
     tf.keras.layers.MaxPooling2D(2, 2),
     
     tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
@@ -183,13 +204,13 @@ if __name__ == '__main__':
   model.summary()
   
   history = model.fit(train_set, train_label,
-                      batch_size = BATCH_SIZE,
-                      epochs = EPOCHS,
-                      steps_per_epoch = int(np.ceil(TRAIN_SIZE / BATCH_SIZE)),
-                      validation_data = (valid_set, valid_label),
-                      validation_steps = int(np.ceil(VALID_SIZE / BATCH_SIZE)))
+                      batch_size=BATCH_SIZE,
+                      epochs=EPOCHS,
+                      steps_per_epoch=int(np.ceil(TRAIN_SIZE / BATCH_SIZE)),
+                      validation_data=(valid_set, valid_label),
+                      validation_steps=int(np.ceil(VALID_SIZE / BATCH_SIZE)))
   
-  print('\nHistory dict:', history.history)
+  print('\n# History dict:', history.history)
   
   import matplotlib.pyplot as plt
   
@@ -215,3 +236,7 @@ if __name__ == '__main__':
   plt.title('Training and Validation Loss')
   # plt.savefig('./foo.png')
   plt.show()
+  
+  print('\n# Evaluate on test data')
+  results = model.evaluate(test_set, test_label, batch_size=BATCH_SIZE)
+  print('Test loss, Test acc:', results)
